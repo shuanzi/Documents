4623c95d7a8265e86ec1f2ebb4b779601150cb58
“shuanzi” <daixiquan@gmail.com>
2014-09-01 13:46:15 +0800

    .

--- code/node_js_test/socketio_test/client.js
+++ code/node_js_test/socketio_test/client.js
@@ -1,4 +1,6 @@
 //client.js
+
+/*
 var io = require('socket.io/node_modules/socket.io-client')();
 
 var client = io.connect('http://192.168.161.65', { 'reconnect': false });
@@ -11,3 +13,14 @@ client.on('message',function (message){
 	console.log(message);
 
 })
+*/
+
+var zmq = require('zmq')
+  , sock = zmq.socket('pull');
+
+sock.connect('tcp://127.0.0.1:3000');
+console.log('Worker connected to port 3000');
+
+sock.on('message', function(msg){
+  console.log('work: %s', msg.toString());
+});
--- code/node_js_test/socketio_test/server.js
+++ code/node_js_test/socketio_test/server.js
@@ -1,4 +1,6 @@
 //server.js
+
+/*
 var io = require('socket.io')();
 
 io.on('connection', function(socket){
@@ -13,5 +15,16 @@ io.on('connection', function(socket){
 
 
 io.listen(3000);
+*/
+
+var zmq = require('zmq')
+  , sock = zmq.socket('push');
+
+//sock.bindSync('tcp://127.0.0.1:3000');
+//console.log('Producer bound to port 3000');
+
+///  console.log('sending work');
+//  sock.send('some work');
+//}, 500);
 
 
